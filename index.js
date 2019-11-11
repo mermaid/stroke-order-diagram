5bf92851552595bd3ff351f3590b8514cca56dd0
Cameron H <cameronholiman@gmail.com>
2019-11-11 16:46:43 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,36 @@
+(async function() {
+  const fs = require('fs')
+  const path = require('path')
+  const args = require('minimist')(process.argv.slice(2), {
+    alias: {
+      dir: ['d'], kanji: ['k']
+    },
+    default: {
+      dir: 'downloaded'
+    }
+  });
+  console.log(args);
+  
+  if (!args.kanji) {
+    console.log('You must specify a kanji to download using --kanji=<kanji[,anotherKanji]>')
+    process.exit(1)
+  }
+  
+  const k = args.kanji;
+
+  for (let k of args.kanji.split(',')) {
+    const diagramGenerator = require('./tsurukame-stroke-order.js')
+  
+    const codePoint = k && k.codePointAt(0).toString(16).padStart(5, '0');
+    const downloadName = path.join(args.dir, `${codePoint}.svg`);
+    
+    console.log(`downloading ${k} to ${downloadName}`)
+  
+    const svgData = await diagramGenerator(k);
+  
+    fs.writeFileSync(path.resolve(downloadName), svgData);
+  }
+
+  console.log('DUNZO!')
+  process.exit(0);
+})();
---
+++ tsurukame-stroke-order.js
@@ -0,0 +1,262 @@
+let isNode = (typeof window === 'undefined')
+
+if (isNode) {
+  module.exports = (kanji) => {
+    return new Promise((resolve, reject) => {
+      const jsdom = require('jsdom');
+      const { JSDOM } = jsdom;
+      const { window } = new JSDOM(`
+      <html>
+      <script type="text/javascript" src="file://${require.resolve('snapsvg')}"></script>
+      <script>
+      document.addEventListener('DOMContentLoaded', function() {
+        window.onModulesLoaded();
+      });
+      </script>
+      </html>`,
+      {
+        runScripts: 'dangerously',
+        resources: 'usable'
+      });
+      global.document = window.document;
+      global.window = window;
+
+      window.onModulesLoaded = async () => {
+        global.Snap = window.Snap
+    
+        resolve(await getKanji(kanji));
+      };
+    })
+  }  
+}
+
+let styles = `
+.stroke_order_diagram--outer_container {
+  width: 100%;
+  overflow-x: auto;
+  overflow-y: hidden;
+}
+
+.stroke_order_diagram--guide_line {
+  fill: none;
+  stroke: #ddd;
+  stroke-width: 3;
+  stroke-linecap: square;
+  stroke-linejoin: square;
+  stroke-dasharray:8, 8;
+}
+
+.stroke_order_diagram--bounding_box {
+  fill: none;
+  stroke: #ddd;
+  stroke-width: 4;
+  stroke-linecap: square;
+  stroke-linejoin: square;
+}
+
+.stroke_order_diagram--current_path {
+  fill: none;
+  stroke: #000;
+  stroke-width: 6;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+}
+
+.stroke_order_diagram--existing_path {
+  fill: none;
+  stroke: #aaa;
+  stroke-width: 6;
+  stroke-linecap: round;
+  stroke-linejoin:round;
+}
+
+.stroke_order_diagram--path_start {
+  fill: rgba(255, 0, 0, 0.7);
+  stroke:none
+}`;
+
+let circleWidth = 8;
+
+let createCss = function() {
+  let css = document.createElement('style');
+  css.innerHTML = styles;
+  return css;
+}
+
+var strokeOrderDiagram = function(element, svgDocument) {
+  element.appendChild(createCss());
+  var s = Snap(element);
+  var diagramSize = 200;
+  var coordRe = '(?:\\d+(?:\\.\\d+)?)';
+  var strokeRe = new RegExp('^[LMT]\\s*(' + coordRe + ')[,\\s](' + coordRe + ')', 'i');
+  var f = Snap(svgDocument);
+  var allPaths = f.selectAll("path");
+  var drawnPaths = [];
+  var canvasWidth = (allPaths.length * diagramSize) / 2;
+  var canvasHeight = diagramSize / 2;
+  var frameSize = diagramSize / 2;
+  var frameOffsetMatrix = new Snap.Matrix()
+  frameOffsetMatrix.translate((-frameSize / 16)+2, (-frameSize / 16)+2);
+
+  // Set drawing area
+  s.node.style.width = canvasWidth + "px";
+  s.node.style.height = canvasHeight + "px";
+  s.node.setAttribute("viewBox", "0 0 " + canvasWidth + " " + canvasHeight);
+
+  // Draw global guides
+  var boundingBoxTop = s.line(1, 1, canvasWidth-1, 1);
+  var boundingBoxLeft = s.line(1, 1, 1, canvasHeight-1);
+  var boundingBoxBottom = s.line(1, canvasHeight-1, canvasWidth-1, canvasHeight-1);
+  var horizontalGuide = s.line(0, canvasHeight/2, canvasWidth, canvasHeight/2);
+  boundingBoxTop.attr({"class": "stroke_order_diagram--bounding_box"});
+  boundingBoxLeft.attr({"class": "stroke_order_diagram--bounding_box"});
+  boundingBoxBottom.attr({"class": "stroke_order_diagram--bounding_box"});
+  horizontalGuide.attr({"class": "stroke_order_diagram--guide_line"});
+
+  // Draw strokes
+  var pathNumber = 1;
+  allPaths.forEach(function(currentPath) {
+    var moveFrameMatrix = new Snap.Matrix()
+    moveFrameMatrix.translate((frameSize * (pathNumber - 1)) - 4, -4);
+
+    // Draw frame guides
+    var verticalGuide = s.line((frameSize * pathNumber) - (frameSize / 2), 1, (frameSize * pathNumber) - (frameSize / 2), canvasHeight-1);
+    var frameBoxRight = s.line((frameSize * pathNumber) - 1, 1, (frameSize * pathNumber) - 1, canvasHeight-1);
+    verticalGuide.attr({"class": "stroke_order_diagram--guide_line"});
+    frameBoxRight.attr({"class": "stroke_order_diagram--bounding_box"});
+
+    // Draw previous strokes
+    drawnPaths.forEach(function(existingPath) {
+      var localPath = existingPath.clone();
+      localPath.transform(moveFrameMatrix);
+      localPath.attr({"class": "stroke_order_diagram--existing_path"})
+      s.append(localPath);
+    });
+
+    // Draw current stroke
+    currentPath.transform(frameOffsetMatrix);
+    currentPath.transform(moveFrameMatrix);
+    currentPath.attr({"class": "stroke_order_diagram--current_path"})
+    s.append(currentPath);
+
+    // Draw stroke start point
+    var match = strokeRe.exec(currentPath.node.getAttribute('d'));
+    var pathStartX = match[1];
+    var pathStartY = match[2];
+    var strokeStart = s.circle(pathStartX, pathStartY, circleWidth);
+    strokeStart.attr({"class": "stroke_order_diagram--path_start"});
+    strokeStart.transform(moveFrameMatrix);
+
+    pathNumber++;
+    drawnPaths.push(currentPath.clone());
+  });
+};
+
+
+
+function initDiagram(response) {
+  var el = document.createElement('svg');
+  
+  new strokeOrderDiagram(el, response);
+
+  return el;
+}
+
+function getDomFromString(string) {
+  var wrapper= document.createElement('div');
+  wrapper.innerHTML = string;
+  return wrapper.getElementsByTagName('svg')[0];
+}
+
+let baseUrl = 'https://raw.githubusercontent.com/KanjiVG/kanjivg/master/kanji/'
+
+function getCodePoint(kanji) {
+  return kanji && kanji.codePointAt(0).toString(16).padStart(5, '0');
+}
+
+function getURL(kanji) {
+  return `${baseUrl}${getCodePoint(kanji)}.svg`
+}
+
+async function getKanji(kanji, download) {
+  return isNode ? getKanjiNode(kanji) : getKanjiBrowser(kanji, download);
+}
+
+async function getKanjiNode(kanji) {
+  return new Promise((resolve, reject) => {
+    require('request')(getURL(kanji), function(err, response, body) {
+      if (err) {
+        reject(err);
+      }
+      let diagram = initDiagram(getDomFromString(body));
+
+      var wrapper= document.createElement('div');
+      wrapper.appendChild(diagram);
+      resolve(wrapper.innerHTML);
+    });
+  })
+}
+
+async function getKanjiBrowser(kanji, download) {
+  return new Promise((resolve, reject) => {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          window.history.pushState('', '', `?kanji=${kanji}`)
+          clearDiagram();
+          let diagram = initDiagram(getDomFromString(this.response));
+  
+          document.getElementById('diagram-container').appendChild(diagram);
+          // gotta self assign innerHTML because browsers are jank and it wont show the element otherwise
+          document.getElementById('diagram-container').innerHTML = document.getElementById('diagram-container').innerHTML;
+  
+          if (download) {
+            let blob = new Blob([document.getElementById('diagram-container').innerHTML], {type: 'image/svg+xml'})
+            let url = window.URL.createObjectURL(blob)
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            // the filename you want
+            a.download =  `${getCodePoint(kanji)}.svg`;
+            // document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+          }
+        }
+    };
+    xhttp.open("GET", getURL(kanji), true);
+    xhttp.send();
+  });
+}
+
+function clearDiagram() {
+  document.getElementById('diagram-container').innerHTML = '';
+}
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+if (!isNode) {
+  document.addEventListener('DOMContentLoaded', function() {
+    let kanji = getParameterByName('kanji');
+    if (kanji) {
+      let dl = getParameterByName('dl');
+      getKanji(kanji, dl === '1');
+    }
+  
+    document.getElementById('diagram-form').addEventListener('submit', function(e) {
+      getKanji($('#kanji').val()[0]);
+      e.preventDefault();
+    })
+    document.getElementById('download-diagram').addEventListener('click', function() {
+      getKanji($('#kanji').val()[0], true);
+    })
+  });
+}
